0982d04658d3ebefad7962ba083956e353dd3ff9
Julian van den Berkmortel <julianvdberkmortel@outlook.com>
2021-10-09 23:42:51 +0200

    Add a test case for the environment info collector

---
+++ collector/environment_info_test.go
@@ -0,0 +1,51 @@
+package collector
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/Serializator/magento2-prometheus-exporter-golang/config"
+)
+
+func TestEnvironmentInfo(t *testing.T) {
+	testHttpServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, `{"version":"2.4.3","edition":"Commerce","mode":"production"}`)
+	}))
+
+	defer testHttpServer.Close()
+
+	testHttpServerUrl, err := url.Parse(testHttpServer.URL)
+	if err != nil {
+		t.Fatalf("Failed to parse URL of the HTTP test server: %s", err)
+	}
+
+	environmentInfo := NewEnvironmentInfoCollector(*http.DefaultClient, config.Config{
+		Magento: struct {
+			Url    string
+			Bearer string
+		}{
+			Url:    testHttpServerUrl.String(),
+			Bearer: "",
+		},
+	})
+
+	environmentInfoResponse, err := environmentInfo.fetchAndDecodeEnvironmentInfo()
+	if err != nil {
+		t.Fatalf("Failed to fetch or decode environment info: %s", err)
+	}
+
+	if environmentInfoResponse.Version != "2.4.3" {
+		t.Errorf("Invalid Magento version response")
+	}
+
+	if environmentInfoResponse.Edition != "Commerce" {
+		t.Errorf("Invalid Magento edition response")
+	}
+
+	if environmentInfoResponse.Mode != "production" {
+		t.Errorf("Invalid Magento mode response")
+	}
+}
